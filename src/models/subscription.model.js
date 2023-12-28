9a3414f5192198520e10243602b60a9a09dd041f
RutvikMendpara <rutvikm200@gmail.com>
2023-12-28 09:35:12 +0530

    Add subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const SubscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId, // one who is subscribing
+      ref: "User",
+      required: true,
+    },
+    channel: {
+      type: Schema.Types.ObjectId, // one who is being subscribed to
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Subscription", SubscriptionSchema);
