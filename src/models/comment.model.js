c1d6018fe486a59aabf99925307e83677bb8c7e0
RutvikMendpara <rutvikm200@gmail.com>
2023-12-31 09:38:20 +0530

    Add Comment model

---
+++ src/models/comment.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    video: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Comment", CommentSchema);
